2d6794d5f8f2b8d177fdf801a7d753a5f9d26c97
Diana <isimetovadiana@yandex.ru>
2025-09-08 20:28:04 +0300

    text

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,44 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
-
+const compare = createComparison(defaultRules);
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes)                                    // Получаем ключи из объекта
+      .forEach((elementName) => {                        // Перебираем по именам
+        elements[elementName].append(                    // в каждый элемент добавляем опции
+            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
+                      .map(name => {
+                        const option = document.createElement('option');
+                        option.value = name;
+                        option.textContent = name;
+                        return option;                        // используйте name как значение и текстовое содержимое                            
+                      })
+        )
+     })
 
     return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+        // @todo: #4.2 — обработать очистку поля(что то не так)
+        if (action && action.name === 'clear') {
+    
+        const parent = action.closest('[data-field]')?.parentElement || 
+                    action.closest('.filter-container') ||
+                    action.parentElement;
+        
+        if (parent) {
+            const input = parent.querySelector('input');
+            if (input) {
+                input.value = '';
+            }
+        }
+        
+        const fieldName = action.dataset.field;
+        if (fieldName) {
+            state[fieldName] = '';
+        }
+    }
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
--- src/components/searching.js
+++ src/components/searching.js
@@ -3,9 +3,16 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
-
+    const comparator = createComparison(
+        rules.skipEmptyTargetValues(), // используем правило из объекта rules
+        rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)
+    );
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        const searchValue = state[searchField];
+        if (!searchValue) return data;
+        
+        return data.filter(item => comparator(item, searchValue));
+        
     }
 }
