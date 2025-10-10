df88eeea47070910c741f0f59bdcfd6398deefa3
Diana <isimetovadiana@yandex.ru>
2025-10-10 18:30:51 +0300

    text

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,18 +1,10 @@
-import {rules, createComparison} from "../lib/compare.js";
-
 
 export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
-    const comparator = createComparison(
-        rules.skipEmptyTargetValues(), // используем правило из объекта rules
-        rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)
-    );
     return (data, state, action) => {
-        // @todo: #5.2 — применить компаратор
-        const searchValue = state[searchField];
-        if (!searchValue) return data;
-        
-        return data.filter(item => comparator(item, searchValue));
-        
+        return (query, state, action) => { // result заменили на query
+            return state[searchField] ? Object.assign({}, query, { // проверяем, что в поле поиска было что-то введено
+            search: state[searchField] // устанавливаем в query параметр
+        }) : query; // если поле с поиском пустое, просто возвращаем query без изменений
+        }
     }
 }
